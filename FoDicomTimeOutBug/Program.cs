201fc300f2b9a243e2c00349af29cc9faca1d74e
Fredrik Carlbom <fredrik.carlbom@sectra.com>
2024-05-03 14:32:45 +0200

    Example of bug

---
+++ FoDicomTimeOutBug/Program.cs
@@ -0,0 +1,17 @@
+﻿using FellowOakDicom.Network;
+using FellowOakDicom.Network.Client;
+
+var client = DicomClientFactory.Create("1.1.1.1", 4242, false, "dummy", "dummy");
+client.ClientOptions.AssociationRequestTimeoutInMs = 1000;
+
+var cEchoRequest = new DicomCEchoRequest {
+    OnTimeout = (_, _) => { Console.WriteLine($"TimeOut {DateTime.Now}"); },
+    OnResponseReceived = (_, response) => { Console.WriteLine($"Response {DateTime.Now}"); }
+};
+await client.AddRequestAsync(cEchoRequest);
+Console.WriteLine($"Sending {DateTime.Now}");
+try {
+    await client.SendAsync();
+}
+catch (AggregateException) {}
+Console.WriteLine($"Done {DateTime.Now}");
